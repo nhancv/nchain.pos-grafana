4250fcff8ceb1773f9b2c10f861073864348f760
Nhan Cao <nhancv92@gmail.com>
2025-05-15 10:28:54 +0700

    feat: gen config dynamically

--- README.md
+++ README.md
@@ -30,21 +30,7 @@
 
 ```
 export CHAIN_ID=evmos_9000-1
-
-export NODE0_PROOF_URL=host.docker.internal:6060
-export NODE1_PROOF_URL=host.docker.internal:6061
-export NODE2_PROOF_URL=host.docker.internal:6062
-export NODE3_PROOF_URL=host.docker.internal:6063
-
-export NODE0_RPC_URL=host.docker.internal:6065
-export NODE1_RPC_URL=host.docker.internal:6066
-export NODE2_RPC_URL=host.docker.internal:6067
-export NODE3_RPC_URL=host.docker.internal:6068
-
-export NODE0_TENDERMINT_URL=host.docker.internal:26660
-export NODE1_TENDERMINT_URL=host.docker.internal:26661
-export NODE2_TENDERMINT_URL=host.docker.internal:26662
-export NODE3_TENDERMINT_URL=host.docker.internal:26663
+export NODE_DOMAIN="domain1 domain2 domain3 domain4"
 
 ./update_urls.sh
 ```
--- update_urls.sh
+++ update_urls.sh
@@ -5,25 +5,47 @@ PROMETHEUS_CONF=$(pwd)/prometheus/prometheus-full.yml
 GRAFANA_COSMOS_CONF=$(pwd)/grafana/provisioning/dashboards/cosmos-dashboard.json
 GRAFANA_NODE_CONF=$(pwd)/grafana/provisioning/dashboards/node-full-dashboard.json
 
-cp $PHLARE_CONF.bk $PHLARE_CONF
-cp $PROMETHEUS_CONF.bk $PROMETHEUS_CONF
+IFS=' ' read -r -a domains <<< "$NODE_DOMAIN"
 
-sed -i.bak "s/host.docker.internal:6060/$NODE0_PROOF_URL/g" "$PHLARE_CONF"
-sed -i.bak "s/host.docker.internal:6061/$NODE1_PROOF_URL/g" "$PHLARE_CONF"
-sed -i.bak "s/host.docker.internal:6062/$NODE2_PROOF_URL/g" "$PHLARE_CONF"
-sed -i.bak "s/host.docker.internal:6063/$NODE3_PROOF_URL/g" "$PHLARE_CONF"
+# Update phlare config header
+echo "scrape_configs:" > $PHLARE_CONF
+phlare_block=$(awk 'NR>1 {print}' $PHLARE_CONF.bk)
+
+# Update prometheus config header
+awk '/scrape_configs:/ {print; exit} {print}' $PROMETHEUS_CONF.bk > $PROMETHEUS_CONF
+awk '/- job_name: '\''prometheus'\''/{flag=1} flag && !/^  - job_name: "node0"/{print} /^  - job_name: "node0"/{flag=0}' $PROMETHEUS_CONF.bk | sed '/^- job_name: "node0"/d' >> $PROMETHEUS_CONF
+# echo "" >> $PROMETHEUS_CONF
+prometheus_block_node=$(awk '/- job_name: "node0"/,/- job_name: "node-rpc0"/{if(!/- job_name: "node-rpc0"/)print}' $PROMETHEUS_CONF.bk)
+prometheus_block_rpc=$(awk '/- job_name: "node-rpc0"/,0' $PROMETHEUS_CONF.bk)
+
+for i in "${!domains[@]}"; do
+  domain="${domains[$i]}"
+  proof_port=$((6060 + i))
+  rpc_port=$((6065 + i))
+  tendermint_port=$((26660 + i))
+
+  # Generate phlare block for each node
+  echo "$phlare_block" | \
+    sed "s/job_name: \"node0\"/job_name: \"node$i\"/" | \
+    sed "s/targets: \\[\"host.docker.internal:6060\"\\]/targets: [\"$domain:$proof_port\"]/" \
+    >> $PHLARE_CONF
+
+  # Update prometheus config for each node
+  # Block node
+  echo "$prometheus_block_node" | \
+    sed "s/job_name: \"node0\"/job_name: \"node${i}\"/" | \
+    sed "s/host.docker.internal:26660/${domain}:${tendermint_port}/" \
+    >> $PROMETHEUS_CONF
+  
+  # Block node-rpc
+  echo "$prometheus_block_rpc" | \
+    sed "s/job_name: \"node-rpc0\"/job_name: \"node-rpc${i}\"/" | \
+    sed "s/host.docker.internal:6065/${domain}:${rpc_port}/" \
+    >> $PROMETHEUS_CONF
+  echo "" >> $PROMETHEUS_CONF
+
+done
 rm -rf "$PHLARE_CONF.bak"
-
-sed -i.bak "s/host.docker.internal:6065/$NODE0_RPC_URL/g" "$PROMETHEUS_CONF"
-sed -i.bak "s/host.docker.internal:6066/$NODE1_RPC_URL/g" "$PROMETHEUS_CONF"
-sed -i.bak "s/host.docker.internal:6067/$NODE2_RPC_URL/g" "$PROMETHEUS_CONF"
-sed -i.bak "s/host.docker.internal:6068/$NODE3_RPC_URL/g" "$PROMETHEUS_CONF"
-rm -rf "$PROMETHEUS_CONF.bak"
-
-sed -i.bak "s/host.docker.internal:26660/$NODE0_TENDERMINT_URL/g" "$PROMETHEUS_CONF"
-sed -i.bak "s/host.docker.internal:26661/$NODE1_TENDERMINT_URL/g" "$PROMETHEUS_CONF"
-sed -i.bak "s/host.docker.internal:26662/$NODE2_TENDERMINT_URL/g" "$PROMETHEUS_CONF"
-sed -i.bak "s/host.docker.internal:26663/$NODE3_TENDERMINT_URL/g" "$PROMETHEUS_CONF"
 rm -rf "$PROMETHEUS_CONF.bak"
 
 sed -i.bak "s/evmos_9000-1/$CHAIN_ID/g" "$GRAFANA_COSMOS_CONF"
